f43d337f4978ea3ff174109550ee9fb41077c9dd
a.argeu@gmail.com <a.argeu@gmail.com@682e29df-f804-3bd9-62a5-24ccd4ff9e5d>
2013-09-25 15:28:32 +0000

    melhora no comprotamento do css

---
+++ RedesConvergentes20132/includes/routerTemplate.jsp
@@ -0,0 +1,12 @@
+<div id="router" style="display: none;">
+	<table>
+		<tr>
+			<td>Endereco IP</td>
+			<td>:ipAddress</td>
+		</tr>
+		<tr>
+			<td>UP Time</td>
+			<td>:upTime</td>
+		</tr>
+	</table>
+</div>
---
+++ RedesConvergentes20132/js/index.js
@@ -0,0 +1,81 @@
+dwr.engine.setActiveReverseAjax(true);
+dwr.engine.setErrorHandler(erroServer);
+var current;
+
+function updateCurrentInterval(ip){
+	current = setInterval(function() {
+		SMNPdwr.getMessage(ip,{
+			callback : function(snmp) {
+				//RouterData
+				var table = $("#router").html();
+				table = table.replace(":ipAddress",snmp.ipAddress);
+				table = table.replace(":upTime",snmp.sysUpTime);
+				//Set the template table for the router
+				var ipId = snmp.ipAddress.split(".").join("");
+				$("#tab"+ipId).html("");
+				$("#tab"+ipId).html(table);
+			},
+			errorHandler : function(message) {
+				clearInterval(current);
+				$("#tab"+ipId).remove();
+				$("#li"+ipId).remove();
+				$("#tabs").tabs("refresh");
+				$("#tabs").tabs("option", "active", -1);
+				alert(message);
+			}
+		});
+	}, 2000);
+}
+
+function erroServer(){
+	clearInterval(current);
+	alert("Não foi possível trazer as informações atualizadas, verifique a sua conexão.");
+}
+
+function isIpValid(ip){
+	if(ip == '' || ip == undefined){
+		alert("Informe um endereço de ip de um dispositivo.");
+		return false;
+	}
+	if(ip.indexOf(".") == -1){
+		alert("O endereço de ip é inválido.");
+		return false;
+	}
+	var ipId = $("#ipAddress").val().split(".").join("");
+	if($("#tab"+ipId).length > 0){
+		alert("O endereço de ip já foi adicionado.");
+		return false;
+	}
+	return true;
+}
+
+function createNewTab() {
+	var ip = $.trim($("#ipAddress").val());
+	if(isIpValid(ip)){
+		if($("#tabs:hidden").length > 0){
+			$("#tabs").show();
+		}
+		var ipId = $("#ipAddress").val().split(".").join("");
+		$("#menu").append("<li id='li"+ipId+"'><a href='#tab"+ipId+"' style='cursor: pointer;' onclick='changeData(this);'>"+ip+"</a><span class='ui-icon ui-icon-close'></span></li>");
+		$("#tabs").append("<div id='tab"+ipId+"'></div>");
+		$("#tabs").tabs("refresh");
+		$("#tabs").tabs("option", "active", -1);
+		clearInterval(current);
+		updateCurrentInterval(ip);
+	}
+}
+
+function changeData(link) {
+	clearInterval(current);
+	var ip = $.trim($(link).html());
+	updateCurrentInterval(ip);
+}
+$(function() {
+	$("#tabs").tabs();
+	$("#tabs").delegate("span.ui-icon-close", "click", function() {
+		clearInterval(current);
+		var panelId = $(this).closest("li").remove().attr("aria-controls");
+		$("#" + panelId).remove();
+		$("#tabs").tabs("refresh");
+	});
+});
